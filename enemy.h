8252f33ef55b8b4f2d941c98676e859b8fe9b554
Fré De Baerdemaeker <fred.debaerdemaeker@gmail.com>
2011-12-08 22:28:14 +0100

    enemy bestanden

---
+++ enemy.h
@@ -0,0 +1,22 @@
+#ifndef H_ENEMY
+#define H_ENEMY
+
+#include "blast_it.h"
+#include "level.h"
+
+/**
+ * Initialiseert de enemy, wijst random locatie toe etc
+ */
+void init_enemy(Enemy* enemy);
+
+/**
+ * Tekent de enemy op het scherm.
+ */
+void render_enemy(Enemy* enemy);
+
+/**
+ * Verwijdert de enemy.
+ */
+void destroy_enemy(Enemy* enemy);
+
+#endif
